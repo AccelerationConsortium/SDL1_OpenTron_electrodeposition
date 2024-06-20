e32cdbd69cda0af5341fc9cffc6f4456cf31805f
nisfisker <nis.fisker@gmail.com>
2024-06-20 14:33:27 -0400

    CP measurements still doesn't work. CV and EIS works

--- src/python/example_biologic_potentiostat.py
+++ src/python/example_biologic_potentiostat.py
@@ -4,9 +4,73 @@ import pandas as pd
 from biologic import connect, BANDWIDTH
 from biologic.techniques.cv import CVTechnique, CVParams, CVStep
 from biologic.techniques.ocv import OCVTechnique, OCVParams
-from biologic.techniques.peis import PEISTechnique, PEISParams
-from biologic.techniques.cp import CPTechnique, CPParams, CPStep
+from biologic.techniques.peis import PEISTechnique, PEISParams, SweepMode
+from biologic.techniques.cp import CPTechnique, CPParams, CPStep, Parameter
 
+########################################################################################
+# Create a CP technique
+print("CP technique")
+params = CPParams(record_every_dE=0.01,
+                  record_every_dT=1,
+                  n_cycles=1,
+                  steps=[CPStep(current=0.1, duration=10, vs_initial=False)],
+                  bandwidth=BANDWIDTH.BW_5,
+                  )
+
+tech = CPTechnique(params)
+
+# Push the technique to the Biologic
+results = []
+with connect('USB0', force_load = True) as bl:
+    channel = bl.get_channel(2)
+    runner = channel.run_techniques([tech])
+    for result in runner:
+        results.append(result.data)
+        # print(result.data)
+    else:
+        time.sleep(1)
+
+# make results into a pandas dataframe
+print("CP results")
+df = pd.DataFrame(results)
+print(df)
+
+########################################################################################
+# Create a EIS technique
+print("EIS technique")
+params = PEISParams(vs_initial=False,
+                    initial_voltage_step=0.1,
+                    duration_step=3,
+                    record_every_dI=0.01,
+                    record_every_dT=1,
+                    correction=False,
+                    final_frequency=1000,
+                    initial_frequency=100000,
+                    amplitude_voltage=0.01,
+                    average_n_times=3,
+                    frequency_number=10,
+                    sweep=SweepMode.Linear, # Linear or Logarithmic
+                    wait_for_steady=False,
+                    bandwidth=BANDWIDTH.BW_5)
+
+
+tech = PEISTechnique(params)
+
+# Push the technique to the Biologic
+results = []
+with connect('USB0', force_load = True) as bl:
+    channel = bl.get_channel(2)
+    runner = channel.run_techniques([tech])
+    for result in runner:
+        results.append(result.data.process_data)
+    else:
+        time.sleep(1)
+
+# make results into a pandas dataframe
+print("EIS results")
+df = pd.DataFrame(results)
+print(df)
+time.sleep(100)
 
 ########################################################################################
 # Create a CV technique
@@ -65,7 +129,7 @@ time.sleep(60)
 # Create a OCV technique
 print("OCV technique")
 params = OCVParams(
-    rest_time_T=20, record_every_dE=0.01,
+    rest_time_T=20, record_every_dE=0.1,
     record_every_dT=1, bandwidth=BANDWIDTH.BW_5,)
 
 tech = OCVTechnique(params)
@@ -87,70 +151,3 @@ df = pd.DataFrame(results)
 print(df)
 time.sleep(60)
 
-
-########################################################################################
-# Create a EIS technique
-print("EIS technique")
-params = PEISParams(vs_initial=False,
-                    initial_voltage_step=0.1,
-                    duration_step=3,
-                    record_every_dI=0.01,
-                    record_every_dT=1,
-                    correction=0,
-                    final_frequency=1000,
-                    initial_frequency=100000,
-                    amplitude_voltage=0.01,
-                    average_n_times=1,
-                    frequency_number=10,
-                    sweep=True,
-                    wait_for_steady=False,
-                    bandwidth=BANDWIDTH.BW_5)
-
-
-
-tech = PEISTechnique(params)
-
-# Push the technique to the Biologic
-results = []
-with connect('USB0', force_load = True) as bl:
-    channel = bl.get_channel(2)
-    runner = channel.run_techniques([tech])
-    for result in runner:
-        results.append(result.data)
-        print(result.data)
-    else:
-        time.sleep(1)
-
-# make results into a pandas dataframe
-print("EIS results")
-df = pd.DataFrame(results)
-print(df)
-time.sleep(60)
-
-########################################################################################
-# Create a CP technique
-print("CP technique")
-params = CPParams(record_every_dE=0.01,
-                  record_every_dT=1,
-                  n_cycles=1,
-                  bandwidth=BANDWIDTH.BW_5,
-                  )
-
-tech = CPTechnique(params)
-
-# Push the technique to the Biologic
-results = []
-with connect('USB0', force_load = True) as bl:
-    channel = bl.get_channel(2)
-    runner = channel.run_techniques([tech])
-    for result in runner:
-        results.append(result.data)
-        print(result.data)
-    else:
-        time.sleep(1)
-
-# make results into a pandas dataframe
-print("CP results")
-df = pd.DataFrame(results)
-print(df)
-# %%
