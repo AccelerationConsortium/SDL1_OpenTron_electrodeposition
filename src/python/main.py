a8b9818e3d103a55cdd002f69f4129b23c1cb60e
nisfisker <nis.fisker@gmail.com>
2024-07-01 14:00:46 -0400

    updating temperature

--- src/python/main.py
+++ src/python/main.py
@@ -36,7 +36,8 @@ experiment = Experiment(
     openTron_IP="100.67.86.197",
     arduino_usb_name="CH340",
 )
-
+experiment.arduino.set_temperature(1, 35)
+exit()
 corrected_potential_10mA = experiment.run_experiment(
     chemicals_to_mix=chemicals_to_mix,
     dispense_ml_electrolyte=3,
