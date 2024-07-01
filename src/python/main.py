77f4caf13186651a19635d3e1c4830146c64502a
nisfisker <nis.fisker@gmail.com>
2024-07-01 12:53:44 -0400

    updating workflow

--- src/python/main.py
+++ src/python/main.py
@@ -21,7 +21,7 @@ chemicals_to_mix = {
 
 # Initialize logging
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
         logging.FileHandler(DATA_PATH + "main.log", mode="a"),
