6729b786a0c9f975765fe9e29748a711adbac6fc
Nis Fisker-Bødker <nis.fisker@gmail.com>
2024-12-10 18:24:25 +0100

    Preparing a reproducability study

--- src/python/main.py
+++ src/python/main.py
@@ -14,15 +14,136 @@ DATA_PATH = ""
 NAME_OF_ARDUINO = "CH340"  # Arduino name on Windows for auto finding COM port
 chemicals_to_mix = [
     {
-        "Ni": 0.10,
-        "Fe": 0.10,
-        "Cr": 0.10,
-        "Mn": 0.10,
-        "Co": 0.10,
-        "Zn": 0.10,
-        "Cu": 0.10,
-        "NH4OH": 0.15,
-        "NaCi": 0.15,
+        "Ni": 0,
+        "Fe": 0,
+        "Cr": 0,
+        "Mn": 0,
+        "Co": 0,
+        "Zn": 0,
+        "Cu": 0,
+        "NH4OH": 0,
+        "NaCi": 0,
+    },
+    {
+        "Ni": 0,
+        "Fe": 0,
+        "Cr": 0,
+        "Mn": 0,
+        "Co": 0,
+        "Zn": 0,
+        "Cu": 0,
+        "NH4OH": 0,
+        "NaCi": 0,
+    },
+    {
+        "Ni": 0,
+        "Fe": 0,
+        "Cr": 0,
+        "Mn": 0,
+        "Co": 0,
+        "Zn": 0,
+        "Cu": 0,
+        "NH4OH": 0,
+        "NaCi": 0,
+    },
+    {
+        "Ni": 0,
+        "Fe": 0,
+        "Cr": 0,
+        "Mn": 0,
+        "Co": 0,
+        "Zn": 0,
+        "Cu": 0,
+        "NH4OH": 0,
+        "NaCi": 0,
+    },
+    {
+        "Ni": 0.5,
+        "Fe": 0.5,
+        "Cr": 0.0,
+        "Mn": 0.0,
+        "Co": 0.0,
+        "Zn": 0.0,
+        "Cu": 0.0,
+        "NH4OH": 0.0,
+        "NaCi": 0.0,
+    },
+    {
+        "Ni": 0.5,
+        "Fe": 0.5,
+        "Cr": 0.0,
+        "Mn": 0.0,
+        "Co": 0.0,
+        "Zn": 0.0,
+        "Cu": 0.0,
+        "NH4OH": 0.0,
+        "NaCi": 0.0,
+    },
+    {
+        "Ni": 0.5,
+        "Fe": 0.5,
+        "Cr": 0.0,
+        "Mn": 0.0,
+        "Co": 0.0,
+        "Zn": 0.0,
+        "Cu": 0.0,
+        "NH4OH": 0.0,
+        "NaCi": 0.0,
+    },
+    {
+        "Ni": 0.5,
+        "Fe": 0.5,
+        "Cr": 0.0,
+        "Mn": 0.0,
+        "Co": 0.0,
+        "Zn": 0.0,
+        "Cu": 0.0,
+        "NH4OH": 0.0,
+        "NaCi": 0.0,
+    },
+    {
+        "Ni": 0.11,
+        "Fe": 0.11,
+        "Cr": 0.11,
+        "Mn": 0.11,
+        "Co": 0.11,
+        "Zn": 0.11,
+        "Cu": 0.11,
+        "NH4OH": 0.11,
+        "NaCi": 0.11,
+    },
+    {
+        "Ni": 0.11,
+        "Fe": 0.11,
+        "Cr": 0.11,
+        "Mn": 0.11,
+        "Co": 0.11,
+        "Zn": 0.11,
+        "Cu": 0.11,
+        "NH4OH": 0.11,
+        "NaCi": 0.11,
+    },
+    {
+        "Ni": 0.11,
+        "Fe": 0.11,
+        "Cr": 0.11,
+        "Mn": 0.11,
+        "Co": 0.11,
+        "Zn": 0.11,
+        "Cu": 0.11,
+        "NH4OH": 0.11,
+        "NaCi": 0.11,
+    },
+    {
+        "Ni": 0.11,
+        "Fe": 0.11,
+        "Cr": 0.11,
+        "Mn": 0.11,
+        "Co": 0.11,
+        "Zn": 0.11,
+        "Cu": 0.11,
+        "NH4OH": 0.11,
+        "NaCi": 0.11,
     },
 ]
 
@@ -42,7 +163,7 @@ time_now = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
 logging.info(f"Initiating potentiostat on port {port} with name {instrument_name}")
 admiral = AdmiralSquidstatWrapper(port=port, instrument_name=instrument_name)
 
-## Priming of pumps
+# Setting temperature on test cell/well (and making an object to do so)
 experiment = Experiment(
     well_volume=2.5,
     cleaning_station_volume=6,
@@ -50,12 +171,15 @@ experiment = Experiment(
     arduino_usb_name="CH340",
 )
 experiment.arduino.set_temperature(1, 35)
-# experiment.cleaning(3)
-# experiment.cleaning(4)
-
 
 logging.info("Sleep 600 seconds to heat up the well plate to 35C")
 time.sleep(300)
+#####################
+# This section is just for a homing run and not necessary for the experiment
+# once all pumps are primed and once the well is 35C
+#####################
+
+# ### Priming of pumps
 # experiment.arduino.dispense_ml(pump=1, volume=2)
 # experiment.arduino.dispense_ml(pump=2, volume=2)
 # experiment.arduino.dispense_ml(pump=3, volume=5)
@@ -95,31 +219,32 @@ time.sleep(300)
 # experiment.save_metadata()
 experiment.__del__()
 
-# tid = [2,10,30,60,90]
+# Loop through all the experiments
+for i in range(len(chemicals_to_mix)):
+    logging.info(f"Starting experiment {i+1}")
 
-logging.info(f"\n\n\nStarting experiment")
-experiment = Experiment(
-    well_volume=2.5,
-    cleaning_station_volume=6,
-    openTron_IP="100.67.86.197",
-    arduino_usb_name="CH340",
-    admiral=admiral,
-)
-experiment.arduino.set_temperature(1, 35)
-corrected_potential_10mA = experiment.run_experiment(
-    chemicals_to_mix=chemicals_to_mix[0], # XXX Change this to match chemicals_to_mix
-    dispense_ml_electrolyte=3,
-    electrodeposition_time=60, # XXX Change this to 2, 10, 30, 60, 90
-    electrodeposition_temperature=35,
-    chemical_ultrasound_mixing_time=30,
-    chemical_rest_time=300,
-)
-experiment.arduino.set_temperature(1, 0)
-experiment.__del__()
+    experiment = Experiment(
+        well_volume=2.5,
+        cleaning_station_volume=6,
+        openTron_IP="100.67.86.197",
+        arduino_usb_name="CH340",
+        admiral=admiral,
+    )
+    experiment.arduino.set_temperature(1, 35)
+    corrected_potential_10mA = experiment.run_experiment(
+        chemicals_to_mix=chemicals_to_mix[i],
+        dispense_ml_electrolyte=3,
+        electrodeposition_time=60,
+        electrodeposition_temperature=35,
+        chemical_ultrasound_mixing_time=30,
+        chemical_rest_time=300,
+    )
+    experiment.arduino.set_temperature(1, 0)
+    experiment.__del__()
 
 logging.info("Closing admiral potentiostat connection")
 admiral.close_experiment()
 
-logging.info(f"Done with experiment \n\n\n")
+logging.info("Done with experiment \n\n\n")
 
 print("Done with experiments\n")
